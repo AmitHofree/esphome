6bb64707f278545eb287d64d7ca505693aa3e1f6
Amit Hofree <amit.hofree@gmail.com>
2024-09-14 15:31:36 +0300

    Initial fully coded version

---
+++ esphome/components/kelvinator_ir/climate.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+from esphome.components import climate_ir
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+AUTO_LOAD = ["climate_ir"]
+CODEOWNERS = ["@amithofre"]
+
+kelvinator_ns = cg.esphome_ns.namespace("kelvinator")
+KelvinatorClimate = kelvinator_ns.class_("KelvinatorClimate", climate_ir.ClimateIR)
+
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(KelvinatorClimate),
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await climate_ir.register_climate_ir(var, config)
